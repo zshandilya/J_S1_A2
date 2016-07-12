dd7f39c81432edf9c793aa00b90a187daa4231f6
zshandilya <zubin_shandilya@yahoo.com>
2016-07-13 01:21:48 +0530

    Assignment 2 for Java Session 1

---
+++ Assignment2.java
@@ -0,0 +1,14 @@
+
+public class Assignment2 {
+	
+	public static void main(String[] args) {
+		
+		int a, b=4, c;
+		a=6;
+		c=b+4;
+		
+		System.out.println("Value of a: " +a);
+		System.out.println("Value of b: " +b);
+		System.out.println("Value of c: " +c);
+	}
+}
